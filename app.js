1c01df382b7b655aa20b70568bbf99066aeab129
erenaci <erenacinoue@gmail.com>
2020-04-20 11:58:40 +0900

    some updates of js

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://afoo01:comp20proj@cluster0-pu67o.mongodb.net/test?\
-           retryWrites=true&w=majority";
+var express=require("express"); 
+var bodyParser=require("body-parser"); 
 
+  
+const MongoClient = require('mongodb').MongoClient; 
+var url = "mongodb+srv://andreafoo11:andreafoo@cluster0-blvfe.mongodb.net/test?\
+          retryWrites=true&w=majority";
+var app = express() 
+var adr = require('url');
+const path = require('path');
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("Music_Events");
-    var currobj = { name_of_event: "Company Inc", genre: "Highway 37", location: "blah"};
-    dbo.collection("Recent_Searches").insertOne(currobj, function(err, res) {
-      if (err) throw err;
-      console.log("updated");
-      db.close();
-    })
+// var searchRouter = require ('./routes/users');
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
+
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ 
+    extended: true
+})); 
+
+app.post('/submit-form', function(req,res){
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("Music_Events");
+        if(req.body.menu == 1) {
+            var currobj = { Artist : req.body.search_value };
+            dbo.collection("Artist_search").insertOne(currobj, function(err, res) {
+              if (err) throw err;
+              console.log("updated");
+              db.close();
+            })
+        }
+        else if (req.body.menu == 2) {
+          var currobj = { Genre : req.body.search_value };
+          dbo.collection("Genre_Search").insertOne(currobj, function(err, res) {
+            if (err) throw err;
+            console.log("updated");
+            db.close();
+          })
+        }
+        else if (req.body.menu == 3) {
+          var currobj = { City : req.body.search_value };
+          dbo.collection("City_Search").insertOne(currobj, function(err, res) {
+            if (err) throw err;
+            console.log("updated");
+            db.close();
+          })
+        }
+        
+  }); 
+});
+  
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
